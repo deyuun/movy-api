21556b3e64e052950c0a9aa9b720aa0c2ae74de1
Dean Harley Pal <paldeanharley@gmail.com>
2026-07-08 18:39:34 +0800

    fix: restore missing jwt import during cleanup

--- modules/auth/controller.js
+++ modules/auth/controller.js
@@ -1,5 +1,6 @@
 import { compare, hash } from 'bcrypt';
 import { User } from '../../models/userModel.js';
+import jwt from 'jsonwebtoken';
 
 export async function registerUser(req, res) {
   try {
